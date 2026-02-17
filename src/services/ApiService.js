a34e5875803e7fbdf7a82dd0c4404a2c6883373c
Priyansh Nihalani <priyansh.nihalani@gmail.com>
2026-02-17 17:27:49 +0530

    [PN][Redirect File Added]

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://rover-assessment-portal-be.onrender.com",
 });
 
 export const ApiService = {
